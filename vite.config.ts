9e25de15a1badbe7ab84f3b483b387dca673d3ed
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2025-06-22 21:03:43 +0800

    Add deployment configs for GitHub Pages and Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/claw-machine-for-jhie/' : '/',
   server: {
     host: "::",
     port: 8080,
